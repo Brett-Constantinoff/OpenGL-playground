5cad46dd6fc1b33d605e620bdceaadc7db0eb7b4
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-02-01 13:32:48 -0700

    adds cube object

--- src/objects/objects.cpp
+++ src/objects/objects.cpp
@@ -1,6 +1,9 @@
 #include "objects.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 Cube::Cube(std::string name){
     this->name = name;
@@ -59,8 +62,69 @@ Cube::Cube(std::string name){
         0.5f, 0.0f, 0.0f, 
         0.5f, 0.0f, 0.5f,
     };
+    this->model = glm::mat4(1.0f);
+    this->genVertexArray();
+    this->bindVertexArray();
+    this->genVertexBuffer();
+    this->bindVertexBuffer();
+    this->setVertexBufferData(this->vertexPositions.size(), this->vertexPositions);
+    this->setVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    this->enableVertexAttrib(0);
 };
 
 void Cube::draw(void){
-    glDrawArrays(GL_TRIANGLES, 0, sizeof(this->vertexPositions) / 3);  
+    this->vao.bind();
+    glDrawArrays(GL_TRIANGLES, 0, this->vertexPositions.size());  
+};
+
+void Cube::move(float x, float y, float z){
+    this->model = glm::translate(this->model, glm::vec3(x, y, z));
+};
+
+void Cube::translate(glm::vec3 vec){
+    this->model *= glm::vec4(vec, 1.0f);
+};
+
+void Cube::genVertexArray(void){
+    this->vao.generate(1);
+};
+
+void Cube::bindVertexArray(void){
+    this->vao.bind();
+};
+
+void Cube::genVertexBuffer(void){
+    this->vertexBuffer.generate(1);
+};
+
+void Cube::genColorBuffer(void){
+    this->colorBuffer.generate(1);
+};
+
+void Cube::bindVertexBuffer(void){
+    this->vertexBuffer.bind();
+};
+
+void Cube::bindColorBuffer(void){
+    this->colorBuffer.bind();
+};
+
+void Cube::setVertexBufferData(size_t size, std::vector<float> data){
+    this->vertexBuffer.setData(size, data);
+};
+
+void Cube::setColorBufferData(size_t size, std::vector<float> data){
+    this->colorBuffer.setData(size, data);
+};
+
+void Cube::setVertexAttribPointer(unsigned int index, int size, int type, bool normalized, int stride, const void* offset){
+    this->vertexBuffer.setAttribPointer(index, size, type, normalized, stride, offset);
+};
+
+void Cube::setColorAttribPointer(unsigned int index, int size, int type, bool normalized, int stride, const void* offset){
+    this->colorBuffer.setAttribPointer(index, size, type, normalized, stride, offset);
+};
+
+void Cube::enableVertexAttrib(unsigned int i){
+    this->vao.enableAttribute(i);
 };
--- src/objects/objects.h
+++ src/objects/objects.h
@@ -3,15 +3,38 @@
 
 #include <string>
 #include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "../buffers/vertexArrayObject.h"
 #include "../buffers/vertexBuffer.h"
 
 class Cube{
+    private:
+        std::vector<float> vertexPositions;
+        VertexArrayObject vao;
+        VertexBuffer vertexBuffer;
+        VertexBuffer colorBuffer;
+        void genVertexArray(void);
+        void bindVertexArray(void);
+        void genVertexBuffer(void);
+        void genColorBuffer(void);
+        void bindVertexBuffer(void);
+        void bindColorBuffer(void);
+        void setVertexBufferData(size_t size, std::vector<float> data);
+        void setColorBufferData(size_t size, std::vector<float> data); 
+        void setVertexAttribPointer(unsigned int index, int size, int type, bool normalized, int stride, const void* offset);
+        void setColorAttribPointer(unsigned int index, int size, int type, bool normalized, int stride, const void* offset);
+        void enableVertexAttrib(unsigned int i);
     public:
         Cube(std::string name);
         std::string name;
-        std::vector<float> vertexPositions;
+        glm::mat4 model;
         void draw(void);
+        void move(float x, float y, float z);
+        void translate(glm::vec3 vec);
+
+       
 };
 
 #endif
