54b49d45110719edb7b15e63ca61a56e0f4f0c37
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-02-01 13:35:58 -0700

    adds cubes to be rendered

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,50 @@
 #include "window/window.h"
 #include "shaders/shader.h"
 #include "objects/objects.h"
-#include "buffers/vertexArrayObject.h"
-#include "buffers/vertexBuffer.h"
+#include "camera/camera.h"
+#include "scene/scene.h"
+#define CUBES 5
+
 
 int main(){
     
    Window window(800, 600, "Hello World");
-   window.init();
    window.setViewPort(0, 0);
    window.enable3d();
 
    Shader shader("shaders/shader.shader");
-
-   Cube cube("my cube");
-
-
-   VertexArrayObject vao;
-   vao.generate(1);
-   vao.bind();
-
-   VertexBuffer vbo;
-   vbo.generate(1);
-   vbo.bind();
-   vbo.setData(cube.vertexPositions.size(), cube.vertexPositions);
-   vbo.setAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-   vao.enableAttribute(0);
-
+   Scene scene;
+    
+   for(int i = 0; i < CUBES; i++){
+       Cube cube("cube" + std::to_string(i));
+       scene.add(cube);
+   }
+   
+   Camera camera(glm::vec3(0.0f, 0.0f ,3.0f), glm::vec3(0.0f, 0.0f, -2.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+   
    while(window.isOpen()){
+       glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+
        window.clearColor(1.0f, 1.0f, 1.0f, 1.0f);
        window.clearBuffers();
 
-       shader.use();
-       vao.bind();
+       camera.move(&window.win);
+       scene.setView(&window, &camera);
 
-       cube.draw();
+       shader.use();
+       shader.setMat4("projection", scene.projection);
+       shader.setMat4("view", scene.view);
+
+       for (int i = 0; i < CUBES; i++){
+           for(Cube c : scene.getObjects()){
+               c.move(position.x, position.y, position.z);
+               shader.setMat4("model", c.model);
+               c.draw();
+               position.x += 1.0f;
+           }
+           position.z += 1.0f;
+           position.x = 0.0f;
+       }
 
        window.swapBuffers();
        window.pollEvents();
