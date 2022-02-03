5f52ce4e7c6326366237f069f0ae2ccf2b7e13c7
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-02-03 12:08:44 -0700

    refactors how the cubes buffers were being organized

--- src/objects/objects.cpp
+++ src/objects/objects.cpp
@@ -1,143 +1,93 @@
 #include "objects.h"
+#include "../../libraries/imageLoading/stb_image.h"
+#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-Cube::Cube(std::string name, glm::vec3 color){
+Cube::Cube(std::string name, glm::vec3 color, const char* texture){
     this->name = name;
     this->color = color;
+    this->texture = texture;
     this->vertexPositions = {
          //front face
-        0.5f, 0.5f, 0.5f, 
-        0.0f, 0.5f, 0.5f, 
-        0.0f, 0.0f, 0.5f, 
-
-        0.0f, 0.0f, 0.5f, 
-        0.5f, 0.0f, 0.5f, 
-        0.5f, 0.5f, 0.5f, 
-
-        //right face
-        0.5f, 0.5f, 0.0f, 
-        0.5f, 0.5f, 0.5f, 
-        0.5f, 0.0f, 0.5f, 
-
-        0.5f, 0.0f, 0.5f, 
-        0.5f, 0.0f, 0.0f,
-        0.5f, 0.5f, 0.0f, 
-
-        //left face
-        0.0f, 0.5f, 0.5f, 
-        0.0f, 0.0f, 0.0f, 
-        0.0f, 0.0f, 0.5f, 
-        
-        0.0f, 0.5f, 0.5f, 
-        0.0f, 0.5f, 0.0f,
-        0.0f, 0.0f, 0.0f,
-
-        //back face
-        0.5f, 0.0f, 0.0f, 
-        0.0f, 0.0f, 0.0f,
-        0.0f, 0.5f, 0.0f, 
-
-        0.0f, 0.5f, 0.0f, 
-        0.5f, 0.5f, 0.0f, 
-        0.5f, 0.0f, 0.0f, 
-
-        //top face
-        0.5f, 0.5f, 0.5f, 
-        0.5f, 0.5f, 0.0f, 
-        0.0f, 0.5f, 0.0f, 
-
-        0.0f, 0.5f, 0.0f, 
-        0.0f, 0.5f, 0.5f, 
-        0.5f, 0.5f, 0.5f,
-
-        //bottom face
-        0.0f, 0.0f, 0.5f, 
-        0.0f, 0.0f, 0.0f, 
-        0.5f, 0.0f, 0.0f, 
-
-        0.0f, 0.0f, 0.5f, 
-        0.5f, 0.0f, 0.0f, 
-        0.5f, 0.0f, 0.5f,
-    };
-
-    this->vertexNormals = {
-         //front face
-        0.0f, 0.0f, 1.0f, 
-        0.0f, 0.0f, 1.0f, 
-        0.0f, 0.0f, 1.0f, 
-
-        0.0f, 0.0f, 1.0f, 
-        0.0f, 0.0f, 1.0f, 
-        0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+        0.0f, 0.5f, 0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f, 
+        0.0f, 0.0f, 0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, 
+        0.0f, 0.0f, 0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+        0.5f, 0.0f, 0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
 
         //right face
-        1.0f, 0.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,
-
-        1.0f, 0.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f,  1.0f, 0.0f, 0.0f,  1.0f, 0.0f,
+        0.5f, 0.0f, 0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
+        0.5f, 0.0f, 0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
+        0.5f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
 
         //left face
-        -1.0f, 0.0f, 0.0f,
-        -1.0f, 0.0f, 0.0f,
-        -1.0f, 0.0f, 0.0f,
-
-        -1.0f, 0.0f, 0.0f,
-        -1.0f, 0.0f, 0.0f,
-        -1.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.5f,  -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,  -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.5f,  -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.5f,  -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,  -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,  -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
 
         //back face
-        0.0f, 0.0f, -1.0f, 
-        0.0f, 0.0f, -1.0f, 
-        0.0f, 0.0f, -1.0f, 
-
-        0.0f, 0.0f, -1.0f, 
-        0.0f, 0.0f, -1.0f, 
-        0.0f, 0.0f, -1.0f,
+        0.5f, 0.0f, 0.0f,  0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,  0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,  0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,  0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.0f,  0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
+        0.5f, 0.0f, 0.0f,  0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
 
         //top face
-        0.0f, 1.0f, 0.0f, 
-        0.0f, 1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-
-        0.0f, 1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+        0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+        0.0f, 0.5f, 0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,
 
         //bottom face
-        0.0f, -1.0f, 0.0f, 
-        0.0f, -1.0f, 0.0f,
-        0.0f, -1.0f, 0.0f,
-
-        0.0f, -1.0f, 0.0f,
-        0.0f, -1.0f, 0.0f,
-        0.0f, -1.0f, 0.0f,
+        0.0f, 0.0f, 0.5f,  0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.0f, 0.0f,  0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.5f,  0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+        0.5f, 0.0f, 0.0f,  0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.0f, 0.5f,  0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
     };
+
     this->model = glm::mat4(1.0f);
     
     this->genVertexArray();
     this->bindVertexArray();
 
+    //set buffer
     this->genBuffer(&this->vertexPosBuffer);
     this->bindBuffer(this->vertexPosBuffer);
     this->setBufferData(this->vertexPositions.size(), this->vertexPositions);
-    this->setAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    this->setTexture();
+
+    //set vertex positions
+    this->setAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     this->enableAttribPointer(0);
 
-    this->genBuffer(&this->vertexNormBuffer);
-    this->bindBuffer(this->vertexNormBuffer);
-    this->setBufferData(this->vertexNormals.size(), this->vertexNormals);
-    this->setAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    //set normals
+    this->setAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     this->enableAttribPointer(1);
+
+    //set uv coordinates
+    this->setAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    this->enableAttribPointer(2);
 };
 
 void Cube::draw(void){
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, this->textBuffer);
     this->bindVertexArray();
     glDrawArrays(GL_TRIANGLES, 0, this->vertexPositions.size());  
 };
@@ -180,3 +130,28 @@ void Cube::setAttribPointer(unsigned int index, int size, int type, bool normali
 void Cube::enableAttribPointer(unsigned int i){
     glEnableVertexAttribArray(i);
 };
+
+void Cube::setTexture(void){
+    glGenTextures(1, &this->textBuffer);
+    glBindTexture(GL_TEXTURE_2D, this->textBuffer);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(this->texture, &width, &height, &nrChannels, 0);
+    if(data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else{
+        std::cout << "Texture loading failed" << std::endl;
+    }
+    stbi_image_free(data);
+};
+
+void Cube::deleteRes(void){
+    glDeleteVertexArrays(1, &this->vao);
+    glDeleteBuffers(1, &this->vertexPosBuffer);
+}
--- src/objects/objects.h
+++ src/objects/objects.h
@@ -11,10 +11,9 @@
 class Cube{
     private:
         std::vector<float> vertexPositions;
-        std::vector<float> vertexNormals;
         unsigned int vao;
         unsigned int vertexPosBuffer;
-        unsigned int vertexNormBuffer;
+        unsigned int textBuffer;
         void genVertexArray(void);
         void bindVertexArray(void);
         void genBuffer(unsigned int* buffer);
@@ -22,18 +21,19 @@ class Cube{
         void setBufferData(size_t size, std::vector<float> data);
         void setAttribPointer(unsigned int index, int size, int type, bool normalized, int stride, const void* offset);
         void enableAttribPointer(unsigned int i);
+        void setTexture(void);
     public:
-        Cube(std::string name, glm::vec3 color);
+        Cube(std::string name, glm::vec3 color, const char* texture);
         glm::vec3 color;
         std::string name;
+        const char* texture;
         glm::mat4 model;
         glm::mat4 normalMat;
         void draw(void);
         void move(float x, float y, float z);
         void updateNormalMat(void);
         void translate(glm::vec3 vec);
-
-       
+        void deleteRes(void);
 };
 
 #endif
