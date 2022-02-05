75f372ebb7b4ceff35084b6ba9335c02103aee02
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-02-05 13:30:27 -0700

    adds init and render function

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -13,7 +13,7 @@ std::vector<SceneObject> Scene::getObjects(void){
 };
 
 void Scene::setView(Window *win, Camera *cam){
-     this->projection = glm::perspective(glm::radians(45.0f), (float)win->getHeight() / (float)win->getWidth(), 1.0f, 100.0f); //creates perspective viewing
+     this->projection = glm::perspective(glm::radians(45.0f), (float)(*win).height/ (float)(*win).width, 1.0f, 100.0f); //creates perspective viewing
      this->view = glm::lookAt(cam->position, cam->position + cam->front, cam->up);
 };
 
@@ -21,4 +21,33 @@ void Scene::deleteScene(void){
     for(SceneObject object : this->objects){
         object.deleteRes();
     }
+};
+
+void Scene::setUp(void){
+    for(int i = 0; i < OBJECTS; i++){
+       glm::vec3 color = glm::vec3(((float)rand() / RAND_MAX), ((float)rand() / RAND_MAX), ((float)rand() / RAND_MAX));
+       Cube cube("cube" + std::to_string(i), color, "../res/brick.jpeg");
+       Pyramid pyramid("pyramid" + std::to_string(i), color, "../res/rock.jpeg");
+       Octahedron octahedron("octahedron" + std::to_string(i), color, "../res/wood.jpeg");
+       this->add(cube);
+       this->add(pyramid);
+       this->add(octahedron);
+   }
+};
+
+void Scene::render(Shader* shader){
+    glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+    for (int i = 0; i < OBJECTS; i++){
+           for(SceneObject object : this->getObjects()){
+               object.move(position.x, position.y, position.z);
+               object.updateNormalMat();
+               shader->setVec3("diffuseVal", object.color);
+               shader->setMat4("model", object.model);
+               shader->setMat4("normalMatrix", object.normalMat);
+               object.draw();
+               position.x += 2.0f;
+           }
+           position.z += 2.0f;
+           position.x = 0.0f;
+       }
 };
--- src/scene/scene.h
+++ src/scene/scene.h
@@ -5,9 +5,13 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "../objects/objects.h"
 #include "../objects/SceneObject.h"
 #include "../window/window.h"
 #include "../camera/camera.h"
+#include "../shaders/shader.h"
+
+#define OBJECTS 10
 
 
 class Scene{
@@ -18,10 +22,11 @@ class Scene{
         glm::mat4 projection;
         glm::mat4 view;
         void add(SceneObject shape);
+        void render(Shader* shader);
         void setView(Window *win, Camera *cam);
         void deleteScene(void);
+        void setUp(void);
         std::vector<SceneObject> getObjects(void);
-
 };
 
 #endif
