24b3dc261d0f0d8967ef9a1a1040ccd3e322ea5f
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-02-05 13:34:18 -0700

    adds new functionality to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,24 @@
 #include "objects/objects.h"
 #include "camera/camera.h"
 #include "scene/scene.h"
-#include <random>
-#include <iostream>
-#define CUBES 10
-
 
 int main(){
     
-   Window window(800, 600, "Playground");
+   Window window(800, 600, "Playground", glm::vec3(0.1f, 0.1f, 0.1f));
    window.enable3d();
 
    Shader shader("shaders/shader.shader");
    Scene scene;
-    
-   for(int i = 0; i < CUBES; i++){
-       glm::vec3 color = glm::vec3(((float)rand() / RAND_MAX), ((float)rand() / RAND_MAX), ((float)rand() / RAND_MAX));
-       Cube cube("cube" + std::to_string(i), color, "../res/brick.jpeg");
-       scene.add(cube);
-   }
+   
+   scene.setUp();
    
    Camera camera(glm::vec3(0.0f, 0.0f ,3.0f), glm::vec3(0.0f, 0.0f, -2.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-   glm::vec3 ambient = glm::vec3(0.1f, 0.1f, 0.1f);
-   glm::vec3 specular = glm::vec3(1.0f, 1.0f, 1.0f);
-   glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
-   glm::vec3 lightPosition = glm::vec3(0.0f, 5.0f, 0.0f);
-   glm::vec3 backgroundCol = glm::vec3(0.1f, 0.1f, 0.1f);
-   const float lightStr = 2.0f;
-   const float n = 2.0f;
+   Light sceneLight(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.1f, 0.1f, 0.1f), glm::vec3(0.5f, 0.5f, 0.5f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 5.0f, 0.0f), 2.0f, 2.0f);
    
    while(window.isOpen()){
-       glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
-       
        camera.move(&window.win);
        scene.setView(&window, &camera);
-       window.clearColor(backgroundCol.r, backgroundCol.g, backgroundCol.b, 1.0f);
+       window.clearColor(window.color.r, window.color.g, window.color.b, 1.0f);
        window.clearBuffers();
        
        //starts gui frame, all gui calls need to happen after this and before the end call
@@ -44,36 +28,24 @@ int main(){
        //main demo window, each gui window can(not neccessary) have its own scope
        {
         window.startGuiElement("Main Window");
-        window.setGuiColor("BackGround Color", &backgroundCol);
+        window.setGuiColor("BackGround Color", &window.color);
         window.displayGuiFloat3("Camera Position", camera.position.x, camera.position.y, camera.position.z);
        }
        //stops gui frame
        window.stopGuiElement();
 
        shader.use();
-       shader.setFloat("lightStr", lightStr);
-       shader.setFloat("n", n);
+       shader.setFloat("lightStr", sceneLight.strength);
+       shader.setFloat("n", sceneLight.n);
        shader.setVec3("cameraPosition", camera.position);
-       shader.setVec3("ambientVal", ambient);
-       shader.setVec3("specularVal", specular);
-       shader.setVec3("lightCol", lightColor);
-       shader.setVec3("lightPos", lightPosition);
+       shader.setVec3("ambientVal", sceneLight.ambient);
+       shader.setVec3("specularVal", sceneLight.specular);
+       shader.setVec3("lightCol", sceneLight.color);
+       shader.setVec3("lightPos", sceneLight.position);
        shader.setMat4("projection", scene.projection);
        shader.setMat4("view", scene.view);
-    
-       for (int i = 0; i < CUBES; i++){
-           for(SceneObject object : scene.getObjects()){
-               object.move(position.x, position.y, position.z);
-               object.updateNormalMat();
-               shader.setVec3("diffuseVal", object.color);
-               shader.setMat4("model", object.model);
-               shader.setMat4("normalMatrix", object.normalMat);
-               object.draw();
-               position.x += 2.0f;
-           }
-           position.z += 2.0f;
-           position.x = 0.0f;
-       }
+
+       scene.render(&shader);
        window.renderGui();
        window.swapBuffers();
        window.pollEvents();
